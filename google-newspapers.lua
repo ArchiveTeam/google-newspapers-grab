856975a800b61f08fc2ddbe146832a83a7d7c317
Arkiver2 <arkiver@protonmail.com>
2018-07-31 17:43:05 +0200

    Abort if status code is not 200 or if /sorry/ in URL. Bump version to 20180731.03.

--- google-newspapers.lua
+++ google-newspapers.lua
@@ -105,34 +105,9 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
     io.stdout:write("ABORTING...\n")
     return wget.actions.ABORT
   end
-  
-  if status_code >= 500 or
-    (status_code >= 400 and status_code ~= 404) or
-    status_code == 0 then
-    io.stdout:write("Server returned "..http_stat.statcode.." ("..err.."). Sleeping.\n")
-    io.stdout:flush()
-    os.execute("sleep 1")
-    tries = tries + 1
-    if tries >= 5 then
-      io.stdout:write("\nI give up...\n")
-      io.stdout:flush()
-      tries = 0
-      if allowed(url["url"], nil) then
-        return wget.actions.ABORT
-      else
-        return wget.actions.EXIT
-      end
-    else
-      return wget.actions.CONTINUE
-    end
-  end
 
-  tries = 0
-
-  local sleep_time = 0
-
-  if sleep_time > 0.001 then
-    os.execute("sleep " .. sleep_time)
+  if status_code ~= 200 or string.match(url["url"], "/sorry/") then
+    return wget.actions.ABORT
   end
 
   return wget.actions.NOTHING
