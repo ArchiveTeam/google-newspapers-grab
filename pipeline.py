e1daf57a5ed0a2d6e14e13d4a82c1985546ea2b4
Arkiver2 <arkiver@protonmail.com>
2018-07-31 16:20:50 +0200

    Remove warc import. Remove warrior-install.sh. Bump version to 20180731.02.

--- pipeline.py
+++ pipeline.py
@@ -17,11 +17,6 @@ import sys
 import time
 import string
 
-try:
-    import warc
-except:
-    raise Exception("Please install warc with 'pip install warc --upgrade'.")
-
 import seesaw
 from seesaw.externalprocess import WgetDownload
 from seesaw.pipeline import Pipeline
@@ -63,7 +58,7 @@ if not WGET_LUA:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20180731.01'
+VERSION = '20180731.02'
 USER_AGENT = 'ArchiveTeam'
 TRACKER_ID = 'google-newspapers'
 TRACKER_HOST = 'tracker.archiveteam.org'
