0702ef1397fbb9220073966321fd0d3066c21c5f
Arkiver2 <arkiver@protonmail.com>
2018-08-13 19:25:45 +0200

    Remove empty line from user-agents.txt. Bump version to 20180813.02.

--- pipeline.py
+++ pipeline.py
@@ -58,7 +58,7 @@ if not WGET_LUA:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20180813.01'
+VERSION = '20180813.02'
 with open('user-agents.txt', 'r') as f:
     USER_AGENT = random.choice(f.read().splitlines()).strip()
 TRACKER_ID = 'google-newspapers'
